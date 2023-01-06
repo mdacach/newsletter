e57d17aa42166bfa23910df27409daca087c002c
Matheus Cardoso <dacachmatheus@gmail.com>
2023-01-06 15:04:56 -0300

    Add mock implementation for subscribe route
    For now, the requests fail (correctly) when the data passed is invalid. Handling the correct data is TODO.

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,23 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+// This tells serde to implement deserialization for us
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+// Actix will try to extract the arguments (in this case web::Form) from the
+// request with from_request. (Internally it will try to deserialize the body
+// into FormData leveraging serde_urlencoded).
+// If this fails (for any argument passed, in this case we only have one),
+// it returns 400 BAD REQUEST
+// otherwise, the arguments are "populated" and the function is invoked
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
@@ -19,10 +36,11 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
             // Note that order here is important, if we had the dynamic /{name} route first,
             // requests to /health_check would match {name}
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
             .route("/{name}", web::get().to(greet))
     })
     .listen(listener)?
-    .run();
+    .run(); // It does not run yet because we have not awaited it
 
     Ok(server)
 }
