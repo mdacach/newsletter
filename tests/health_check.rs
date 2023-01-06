ccbe4cf7aade388a94754a415c935d40e004fbbc
Matheus Cardoso <dacachmatheus@gmail.com>
2023-01-06 12:30:38 -0300

    Add tests for subscriptions path

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,20 +1,6 @@
 use std::net::TcpListener;
 
-#[tokio::test]
-async fn health_check_works() {
-    let address = spawn_app();
-    // Now the server is running and we can proceed with our test logic
-
-    let client = reqwest::Client::new();
-    let response = client
-        .get(&format!("{}/health_check", &address))
-        .send()
-        .await
-        .expect("Failed to execute request.");
-
-    assert!(response.status().is_success());
-    assert_eq!(response.content_length(), Some(0));
-}
+use tokio::spawn;
 
 fn spawn_app() -> String {
     // Bind to a random available port
@@ -28,4 +14,70 @@ fn spawn_app() -> String {
 
     // Return the port so that our test knows where to request
     format!("http://127.0.0.1:{}", port)
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    let app_address = spawn_app();
+    // Now the server is running and we can proceed with our test logic
+
+    let client = reqwest::Client::new();
+    let response = client
+        .get(&format!("{}/health_check", &app_address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(response.content_length(), Some(0));
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40";
+
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missin() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Table-driven tests:
+    // https://github.com/golang/go/wiki/TableDrivenTests
+    // https://dave.cheney.net/2019/05/07/prefer-table-driven-tests
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        )
+    }
 }
