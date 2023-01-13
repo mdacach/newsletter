86c8be9068230217d36c97916bba3cafd51dd94a
Matheus Cardoso <dacachmatheus@gmail.com>
2023-01-13 14:37:00 -0300

    Connect to database in test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,8 @@
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgConnection};
+
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 fn spawn_app() -> String {
@@ -35,6 +38,15 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_string = configuration.database.connection_string();
+    // The `Connection` trait must be in scope for us to invoke
+    // `PgConnection::connect` - it is not an inherent method of the struct!
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40";
