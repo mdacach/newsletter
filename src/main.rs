91d2e700214dded50a041b0fcc45784ed37b4df7
Matheus Cardoso <dacachmatheus@gmail.com>
2023-01-14 19:13:04 -0300

    Add db connection as shared state in Actix

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 use std::net::TcpListener;
 
+use sqlx::{Connection, PgConnection};
+
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
 
     let address = format!("localhost:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -2,17 +2,31 @@ use std::net::TcpListener;
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
 
 use crate::routes::health_check;
 use crate::routes::subscribe;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    // First create the shareable state, and then move inside the closure
+    // otherwise you would create it multiple times, every time the closure
+    // runs.
+    // web::Data is an ARC, so we can clone it inside the closure
+    let connection = web::Data::new(connection);
+
+    // HttpServer receives a closure returning an App
+    // It will call this closure in multiple threads (to create a multi-threaded
+    // web server).
+    // This means anything inside the closure (the connection in this case), must be
+    // shareable between threads, which is not the case of PgConnection (as it sits on
+    // top of a TCP connection itself).
+    let server = HttpServer::new(move || {
         App::new()
             // Note that order here is important, if we had a dynamic /{name} route first,
             // requests to /health_check would match {name}
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection.clone()) // Here we pass a clone
     })
     .listen(listener)?
     .run(); // It does not run yet because we have not awaited it
