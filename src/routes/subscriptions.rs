9b09253ff1609d7e63dea5869cf33eac0ce81d6a
Matheus Cardoso <dacachmatheus@gmail.com>
2023-01-20 13:01:35 -0300

    Use a connection pool for Postgres
    It is not allowed to run concurrent queries on a single DB connection. We could solve this by:
    1. Wrap the connection into a Mutex (but then we will need to wait to lock() the Mutex)
    2. Use a pool of connections
    
    The pool of connections will handle passing us a connection. When the request needs a connection, the pool will either create a new one (and give us exclusive access) or wait for one to be available.
    This way we pay the cost of *establishing* a connection just when needed. (Amortized it's better)

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,7 @@
 use actix_web::{web, HttpResponse, Responder};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 // This tells serde to implement deserialization for us
 #[derive(serde::Deserialize)]
@@ -13,6 +16,24 @@ pub struct FormData {
 // If this fails (for any argument passed, in this case we only have one),
 // it returns 400 BAD REQUEST
 // otherwise, the arguments are "populated" and the function is invoked
-pub async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
-    HttpResponse::Ok().finish()
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
+    // This only runs when we execute it with some connection
+    let insert_query = sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)"#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    );
+
+    // This is a result, as the query may fail
+    match insert_query.execute(pool.get_ref()).await {
+        Ok(_) => HttpResponse::Ok(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError()
+        }
+    }
 }
--- src/startup.rs
+++ src/startup.rs
@@ -2,17 +2,17 @@ use std::net::TcpListener;
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
-use sqlx::PgConnection;
+use sqlx::PgPool;
 
 use crate::routes::health_check;
 use crate::routes::subscribe;
 
-pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // First create the shareable state, and then move inside the closure
     // otherwise you would create it multiple times, every time the closure
     // runs.
     // web::Data is an ARC, so we can clone it inside the closure
-    let connection = web::Data::new(connection);
+    let db_pool = web::Data::new(db_pool);
 
     // HttpServer receives a closure returning an App
     // It will call this closure in multiple threads (to create a multi-threaded
@@ -26,7 +26,7 @@ pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, st
             // requests to /health_check would match {name}
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
-            .app_data(connection.clone()) // Here we pass a clone
+            .app_data(db_pool.clone()) // Here we pass a clone
     })
     .listen(listener)?
     .run(); // It does not run yet because we have not awaited it
