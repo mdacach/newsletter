6d27a49ecae905f99aa306411dac12c8309edcd7
Matheus Cardoso <dacachmatheus@gmail.com>
2023-01-23 20:58:52 -0300

    Refactor subscription code and use tracing instrument macro
    Now we factor out the "inserting" of a user from subscribe. We also use `tracing::instrument` macro to wrap the functions in a span and remove the logging code from inside the functions.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -17,22 +17,25 @@ pub struct FormData {
 // If this fails (for any argument passed, in this case we only have one),
 // it returns 400 BAD REQUEST
 // otherwise, the arguments are "populated" and the function is invoked
-pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
-    let request_id = Uuid::new_v4();
-    // Spans are more powerful, have related metadata and can be entered/exited multiple times
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id, // % means to use Display
-        subscriber_email = %form.email,
-        subscriber_name = %form.name
-    );
-    // Using `enter` in an async function is not great because it can be polled
-    // (and parked) multiple times.
-    let _request_span_guard = request_span.enter(); // The span only starts working when we enter here
-                                                    // and it will be closed when we drop this guard (similar to C++ RAII)
+#[tracing::instrument(name = "Adding a new subscriber",
+skip(form, pool),
+fields(
+request_id = % Uuid::new_v4(),
+subscriber_email = % form.email,
+subscriber_name = % form.name
+))]
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    match insert_subscriber(&pool, &form).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
 
-    let query_span = tracing::info_span!("Saving new subscriber details in the database.");
-
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
     // This only runs when we execute it with some connection
     let insert_query = sqlx::query!(
         r#"
@@ -44,28 +47,10 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> im
         Utc::now()
     );
 
-    // This is a result, as the query may fail
-    match insert_query
-        .execute(pool.get_ref())
-        .instrument(query_span)
-        .await
-    {
-        Ok(_) => {
-            tracing::info!(
-                "request_id {} - New subscriber details have been saved",
-                request_id
-            );
-            HttpResponse::Ok()
-        }
-        Err(e) => {
-            // We use debug formatting here because we want access to more information
-            // about the error in the logs.
-            tracing::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
-            HttpResponse::InternalServerError()
-        }
-    }
+    insert_query.execute(pool).await.map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+
+    Ok(())
 }
