96e8201a3fce9fbdb2afaf8efb0b661bff1f6f5b
Matheus Cardoso <dacachmatheus@gmail.com>
2023-01-22 21:21:14 -0300

    Refactor telemetry stuff into own file

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,15 @@
 use std::net::TcpListener;
 
 use sqlx::PgPool;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    LogTracer::init().expect("Failed to set logger.");
-    // Filter spans depending on level. If no level is designated (with RUST_LOG), set it to "info".
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    // Format the spans in the `bunyan` format (can then be used as JSON).
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".to_string(), std::io::stdout);
-    // This is our actual "implementation".
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer); // We can build it from layers
-    set_global_default(subscriber).expect("Failed to set subscriber.");
+    let subscriber = telemetry::get_subscriber("zero2prod".to_string(), "info".to_string());
+    telemetry::init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
