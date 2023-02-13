19a221ebd5745c90bed1c6aa0aa4a636649b3068
Matheus Cardoso <dacachmatheus@gmail.com>
2023-02-13 11:45:28 -0300

    Add SubscriberEmail, similar to SubscriberName

---
+++ src/domain/subscriber_email.rs
@@ -0,0 +1,43 @@
+#[derive(Debug)]
+pub struct SubscriberEmail(String);
+
+impl SubscriberEmail {
+    pub fn parse(s: String) -> Result<SubscriberEmail, String> {
+        if validator::validate_email(&s) {
+            Ok(Self(s))
+        } else {
+            Err(format!("{} is not a valid subscriber email.", s))
+        }
+    }
+}
+
+impl AsRef<str> for SubscriberEmail {
+    fn as_ref(&self) -> &str {
+        &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use claim::assert_err;
+
+    use super::SubscriberEmail;
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let email = "".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "dacachmatheusgmail.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email = "@gmail.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+}
