a16ae25a457de555cab4ee9b6873afe5d3a0f80b
Matheus Cardoso <dacachmatheus@gmail.com>
2023-02-13 12:25:13 -0300

    Add property-based testing to SubscriberEmail
    Using `quickcheck` together with `fake` we can generate multiple valid emails and check that they all pass.

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -22,8 +22,28 @@ mod tests {
     use claim::{assert_err, assert_ok};
     use fake::faker::internet::en::FreeEmail;
     use fake::Fake;
+    use quickcheck::Gen;
+
+    use rand::rngs::StdRng;
+    use rand::SeedableRng;
 
     use super::SubscriberEmail;
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(g: &mut Gen) -> Self {
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+            let email = FreeEmail().fake_with_rng(&mut rng);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) {
+        assert_ok!(SubscriberEmail::parse(valid_email.0));
+    }
 
     #[test]
     fn empty_string_is_rejected() {
@@ -41,11 +61,5 @@ mod tests {
     fn email_missing_subject_is_rejected() {
         let email = "@gmail.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
-    }
-
-    #[test]
-    fn valid_emails_are_parsed_successfully() {
-        let email = FreeEmail().fake();
-        assert_ok!(SubscriberEmail::parse(email));
     }
 }
