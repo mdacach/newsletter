3d4b451873b171a6c0af2c077ac0ebaceeabb049
Matheus Cardoso <dacachmatheus@gmail.com>
2023-02-22 13:44:29 -0300

    Add explanatory comments to helpers.rs

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -9,19 +9,20 @@ use newsletter::startup::get_connection_pool;
 use newsletter::startup::Application;
 use newsletter::telemetry;
 
-// This should only run one time, not once for each test
-// So we wrap it within `once_cell`
+// We should only register a subscriber once.
+// If we simply have this inside each test, it would be called multiple times.
+// Using `once_cell` solves this issue.
 static TRACING: sync::Lazy<()> = sync::Lazy::new(|| {
     let default_filter_level = "info".to_string();
     let subscriber_name = "test".to_string();
 
-    // We have the option of printing the logs when testing too
+    // This subscriber will keep logging stuff. If we are just running routine tests, this is
+    // unnecessary. So by default, if the flag is not set, we will sink the logs ("drop" them).
     if std::env::var("TEST_LOG").is_ok() {
         let subscriber =
             telemetry::get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
         telemetry::init_subscriber(subscriber);
     } else {
-        // By default we will just ignore them
         let subscriber =
             telemetry::get_subscriber(subscriber_name, default_filter_level, std::io::sink);
         telemetry::init_subscriber(subscriber);
@@ -35,9 +36,11 @@ pub struct TestApp {
 }
 
 impl TestApp {
+    // Performs a post request to subscriptions.
     pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
         reqwest::Client::new()
             .post(&format!("{}/subscriptions", &self.address))
+            // x-www-form-urlencoded is a good way to encode information from Forms.
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(body)
             .send()
@@ -47,11 +50,17 @@ impl TestApp {
 }
 
 pub async fn spawn_app() -> TestApp {
-    // Runs only if it's the first time
+    // Due to `once_cell`, this only runs once, even if we call it multiple times.
     sync::Lazy::force(&TRACING);
 
+    // The configuration for tests has two details:
+    // 1. The database name is randomized. This way we create "temporary" databases, for testing only.
+    //    -> This is good for test isolation.
+    // 2. The port is set to 0. This way the OS will set a random port.
+    //    -> This is good for running multiple tests at the same time.
     let configuration = {
         let mut configuration = get_configuration().expect("Failed to read configuration.");
+
         // As we need test isolation between the tests, we are going to create a new logical database
         // for each test. This way, tests won't interfere with each other, as each one will use a different
         // database.
@@ -62,6 +71,8 @@ pub async fn spawn_app() -> TestApp {
         configuration
     };
 
+    // Note that it is not necessary to use the `connection_pool` returned here, as we have a
+    // specific function for that: `get_connection_pool`.
     configure_database(&configuration.database).await;
 
     let application = Application::build(&configuration)
@@ -69,10 +80,10 @@ pub async fn spawn_app() -> TestApp {
         .expect("Failed to build application.");
     let application_port = application.port();
     let address = format!("http://127.0.0.1:{}", application.port());
-    let _ = tokio::spawn(application.run_until_stopped()); // We are not doing anything to the handle
 
-    // Return the port so that our tests knows where to request
-    // And the pool handle so that they can access the connections
+    // This makes the server run in another thread.
+    let _ = tokio::spawn(application.run_until_stopped());
+
     TestApp {
         port: application_port,
         address,
@@ -81,26 +92,28 @@ pub async fn spawn_app() -> TestApp {
 }
 
 async fn configure_database(config: &DatabaseSettings) -> PgPool {
-    // Establish the connection to Postgres
-    // Note that here we do not use the database name, as we want to connect to Postgres directly
-    // But the database exists, and it is either
-    // 1 - our app database name
-    // 2 - a random name for testing purposes only
+    // By using `connection_string_without_db`, we connect to postgres directly, and can create
+    // a new database below.
     let mut connection =
         PgConnection::connect(config.connection_string_without_db().expose_secret())
             .await
             .expect("Failed to connect to Postgres.");
-    // Create the initial database
+
+    // Create a randomized database.
+    // As each test calls `configure_database` (because it is inside `spawn_app`), each test will
+    // have a unique database to query.
     connection
         .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
         .await
         .expect("Failed to create database.");
 
-    // Migrate database with file we have saved
-    // This will create our needed table
+    // We have just created a database with this same name, so `connection_string` is sufficient now.
     let connection_pool = PgPool::connect(config.connection_string().expose_secret())
         .await
         .expect("Failed to connect to Postgres.");
+
+    // Our database is a clean slate right now.
+    // Migrate it so that it has the same tables as our production one.
     sqlx::migrate!("./migrations")
         .run(&connection_pool)
         .await
