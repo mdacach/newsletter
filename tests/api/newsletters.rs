f52b7ca82071ccd614dbf14b75a8dd801bcfdc3c
Matheus Cardoso <dacachmatheus@gmail.com>
2023-02-28 17:14:13 -0300

    Only accept posts to newsletters endpoint from Authorized users

--- tests/api/newsletters.rs
+++ tests/api/newsletters.rs
@@ -32,3 +32,24 @@ async fn newsletters_returns_400_for_invalid_data() {
         );
     }
 }
+
+#[tokio::test]
+async fn requests_missing_authorization_are_rejected() {
+    let app = helpers::spawn_app().await;
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&serde_json::json!({
+            "title": "Newsletter title",
+            "content": "some content"
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(response.status().as_u16(), 401);
+    assert_eq!(
+        response.headers()["WWW-Authenticate"],
+        r#"Basic realm="publish""#
+    );
+}
