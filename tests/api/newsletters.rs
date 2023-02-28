edf8fdcb9c026592560a3075561e02f937b7d293
Matheus Cardoso <dacachmatheus@gmail.com>
2023-02-28 20:58:53 -0300

    Add tests for user authentication

--- tests/api/newsletters.rs
+++ tests/api/newsletters.rs
@@ -1,3 +1,5 @@
+use uuid::Uuid;
+
 use crate::helpers;
 
 #[tokio::test]
@@ -39,6 +41,58 @@ async fn requests_missing_authorization_are_rejected() {
 
     let response = reqwest::Client::new()
         .post(&format!("{}/newsletters", &app.address))
+        .json(&serde_json::json!({
+            "title": "Newsletter title",
+            "content": "some content"
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(response.status().as_u16(), 401);
+    assert_eq!(
+        response.headers()["WWW-Authenticate"],
+        r#"Basic realm="publish""#
+    );
+}
+
+#[tokio::test]
+async fn non_existing_user_is_rejected() {
+    let app = helpers::spawn_app().await;
+
+    let username = Uuid::new_v4().to_string();
+    let password = Uuid::new_v4().to_string();
+
+    let response = reqwest::Client::new()
+        .post(format!("{}/newsletters", &app.address))
+        .basic_auth(username, Some(password))
+        .json(&serde_json::json!(
+            {
+                "title": "Newsletter title",
+            "content": "some content"
+            }
+        ))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(response.status().as_u16(), 401);
+    assert_eq!(
+        response.headers()["WWW-Authenticate"],
+        r#"Basic realm="publish""#
+    )
+}
+
+#[tokio::test]
+async fn invalid_password_is_rejected() {
+    let app = helpers::spawn_app().await;
+    let username = &app.test_user.username;
+    let random_password = Uuid::new_v4().to_string();
+    assert_ne!(app.test_user.password, random_password);
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .basic_auth(username, Some(random_password))
         .json(&serde_json::json!({
             "title": "Newsletter title",
             "content": "some content"
