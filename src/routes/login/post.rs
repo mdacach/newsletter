8624b63eb89da6886df5b47be101c6e5c36fbe7f
Matheus Cardoso <dacachmatheus@gmail.com>
2023-03-08 14:28:16 -0300

    Remove query error param for login
    We will be using cookies instead.

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -4,14 +4,12 @@ use actix_web::error::InternalError;
 use actix_web::http::header::LOCATION;
 use actix_web::web;
 use actix_web::HttpResponse;
-use hmac::{Hmac, Mac};
-use secrecy::{ExposeSecret, Secret};
+use secrecy::Secret;
 use sqlx::PgPool;
 
 use crate::authentication;
 use crate::authentication::validate_credentials;
 use crate::routes::error_chain_fmt;
-use crate::startup::HMACSecret;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -20,13 +18,12 @@ pub struct FormData {
 }
 
 #[tracing::instrument(
-skip(form, pool, secret),
+skip(form, pool),
 fields(username = tracing::field::Empty, user_id = tracing::field::Empty)
 )]
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-    secret: web::Data<HMACSecret>,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = authentication::Credentials {
         username: form.0.username,
@@ -52,19 +49,8 @@ pub async fn login(
                 }
             };
 
-            let query_string = format!("error={}", urlencoding::Encoded::new(error.to_string()));
-            let hmac_tag = {
-                let secret_key = secret.0.expose_secret().as_bytes();
-                let mut mac = Hmac::<sha2::Sha256>::new_from_slice(secret_key).unwrap();
-                mac.update(query_string.as_bytes());
-                mac.finalize().into_bytes()
-            };
-
             let response = HttpResponse::SeeOther()
-                .insert_header((
-                    LOCATION,
-                    format!("/login?{}&tag={:x}", query_string, hmac_tag),
-                ))
+                .insert_header((LOCATION, "/login"))
                 .finish();
             Err(InternalError::from_response(error, response))
         }
