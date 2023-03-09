087abd63db9423b7f50562aadaf5492896222bdc
Matheus Cardoso <dacachmatheus@gmail.com>
2023-03-09 17:22:24 -0300

    Add seed admin user
    We need to have access to the admin table in some way!

---
+++ migrations/20230309201437_seed_user.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES ('867b8cf6-9aa9-4789-bbc7-4739ba4da25c',
+        'admin',
+        '$argon2id$v=19$m=15000,t=2,p=1$YyhccLlSzg+NcIsWINWaBA$tDpV7/HIfd9nsEcpB+Hwca8bvVp3WRjUgh7Td67MMlc');
