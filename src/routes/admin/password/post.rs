c6239a65cf138933e4509a563a0a1efea0a5ed79
Matheus Cardoso <dacachmatheus@gmail.com>
2023-03-09 18:59:11 -0300

    Handle the case when new password check does not match

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,5 +1,6 @@
 use actix_web::{web, HttpResponse};
-use secrecy::Secret;
+use actix_web_flash_messages::FlashMessage;
+use secrecy::{ExposeSecret, Secret};
 
 use crate::session_state::TypedSession;
 use crate::utils::{opaque_error_500, see_other};
@@ -17,6 +18,16 @@ pub async fn change_password(
 ) -> Result<HttpResponse, actix_web::Error> {
     if session.get_user_id().map_err(opaque_error_500)?.is_none() {
         return Ok(see_other("/login"));
+    }
+
+    let new_password = form.new_password.expose_secret();
+    let new_password_check = form.new_password_check.expose_secret();
+    if new_password != new_password_check {
+        FlashMessage::error(
+            "You entered two different new passwords - the field values must match.",
+        )
+        .send();
+        return Ok(see_other("/admin/password"));
     }
     todo!()
 }
