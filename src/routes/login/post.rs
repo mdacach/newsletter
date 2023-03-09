6a2ab9caf33e6ba5f4afaf53dbfadc63f315bff6
Matheus Cardoso <dacachmatheus@gmail.com>
2023-03-09 16:44:52 -0300

    Prevent session fixation attacks by renewing the session at log in

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -38,6 +38,8 @@ pub async fn login(
         Ok(user_id) => {
             tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
 
+            // Prevent session fixation attacks.
+            session.renew();
             session
                 .insert("user_id", user_id)
                 .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
